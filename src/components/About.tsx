170e9b24822d37d335d836df33fbf58eb2429b47
douglasedward <aiolosds@gmail.com>
2024-12-18 05:24:54 -0400

    switched lottie dependency

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,9 +1,17 @@
-import Lottie from "lottie-react";
+import Lottie from "react-lottie";
 import animationData from "@/assets/lottie/floating_coding.json";
 
 const About = () => {
+  const defaultOptions = {
+    loop: true,
+    autoplay: true,
+    animationData: animationData,
+    rendererSettings: {
+      preserveAspectRatio: "xMidYMid slice",
+    },
+  };
   return (
-    <div className="mx-auto xl:px-44 lg:px-36 md:px-32 sm:px-28 vvs:px-12 px-10 lg:my-32 my-16 space-y-16">
+    <div className="mx-auto xl:px-44 lg:px-36 md:px-32 sm:px-28 vvs:px-12 px-10 lg:my-32 my-16 space-y-16 lg:space-y-10">
       <h1
         id="about-heading"
         className="text-white/80 font-gilmer-outlined lg:mb-10 mb-5 text-[3.5rem] sm:text-[5.2rem] md:text-[6.4rem] lg:text-[7.5rem] leading-[3.5rem] sm:leading-[5.5rem] md:leading-[6rem] lg:leading-[7rem] xl:leading-[9rem] 2xl:leading-[10rem]  cursor-default"
@@ -45,9 +53,14 @@ const About = () => {
         <div>
           <div
             id="lottie"
-            className="xl:!h-[400px] !z-10 xl:!w-[600px] lg:!h-[280px] lg:!w-[480px] md:!h-[240px] md:!w-[300px] !h-[180px] !w-[360px]"
+            className="!z-10 xl:!h-[500px] xl:!w-[600px] lg:!h-[400px] lg:!w-[480px] !h-[300px] !w-[360px]"
           >
-            <Lottie animationData={animationData} loop={true} />
+            <Lottie
+              options={defaultOptions}
+              // height={400}
+              // width={400}
+              isClickToPauseDisabled
+            />
           </div>
         </div>
       </div>
