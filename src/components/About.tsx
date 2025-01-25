1abb1b557de5d4b24ffba873f7277ce3b296b665
douglasedward <aiolosds@gmail.com>
2025-01-25 02:50:39 -0400

    margin tweak

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -3,7 +3,7 @@ import animationData from "@/assets/lottie/floating_coding.json";
 
 const About = () => {
   return (
-    <div className="mx-auto xl:px-44 lg:px-36 md:px-32 sm:px-28 vvs:px-12 px-10 lg:my-32 my-16 space-y-16 lg:space-y-10">
+    <div className="mx-auto xl:px-44 lg:px-36 md:px-32 sm:px-28 vvs:px-12 px-10 lg:my-28 my-16 space-y-16 lg:space-y-10">
       <h1
         id="about-heading"
         className="text-white/80 font-gilmer-outlined lg:mb-10 mb-5 text-[3.5rem] sm:text-[5.2rem] md:text-[6.4rem] lg:text-[7.5rem] leading-[3.5rem] sm:leading-[5.5rem] md:leading-[6rem] lg:leading-[7rem] xl:leading-[9rem] 2xl:leading-[10rem]  cursor-default"
@@ -45,7 +45,7 @@ const About = () => {
         <div>
           <div
             id="lottie"
-            className="xl:!h-[400px] !z-10 xl:!w-[600px] lg:!h-[280px] lg:!w-[480px] md:!h-[240px] md:!w-[300px] !h-[180px] !w-[360px]"
+            className="xl:!h-[400px] !z-10 xl:!w-[600px] lg:!h-[280px] lg:!w-[480px] md:!h-[240px] md:!w-[300px] !h-[180px] !w-[360px] md:mb-0 mb-12"
           >
             <Lottie animationData={animationData} loop={true} />
           </div>
