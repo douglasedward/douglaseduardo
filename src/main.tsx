d53f7b81c689415cdda919b14a72d6a58482ff44
douglasedward <aiolosds@gmail.com>
2025-02-17 15:01:27 -0400

    init i18n config and checking for previous saved language

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,18 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
+import "./lib/i18n";
 
-createRoot(document.getElementById('root')!).render(
+const savedLang = localStorage.getItem("lng");
+if (savedLang) {
+  import("./lib/i18n").then(({ default: i18n }) =>
+    i18n.changeLanguage(savedLang)
+  );
+}
+
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <App />
-  </StrictMode>,
-)
+  </StrictMode>
+);
